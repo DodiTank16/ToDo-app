3932cbc9b55821f6b968f3299fa64b3b57ff2400
TankDodi16 <tankdodi@gmail.com>
2022-02-04 17:25:00 +0530

    Data Consoled but not Fetched in Page1

--- src/Components/Page1.js
+++ src/Components/Page1.js
@@ -1,15 +1,13 @@
 import { Button, Paper } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import "../index.css";
 import { Link, useLocation } from "react-router-dom";
 // import Page2 from "./Page2";
 
 export default function Page1(props) {
 	const location = useLocation();
-	const { title1, desc, priority1 } = location.state;
-
-	// const {title, desc, priority} = props;
-	// console.log(props);
+	const { tododata } = location.state;
+	console.log(tododata);
 	return (
 		<>
 			<Paper
@@ -19,9 +17,7 @@ export default function Page1(props) {
 				<div className="todoApp">
 					<h1>Todo Application</h1>
 					<ul className="list">
-						<li>
-							{title1}
-						</li>
+						<li>{tododata}</li>
 					</ul>
 					<Link to={"/Page2"} style={{ textDecoration: "none" }}>
 						<Button variant="contained">Add</Button>
